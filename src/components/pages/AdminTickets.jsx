4030b236de277af891922aea1c9114436d1abb55
dhanasriganesh <dhanasriganesh9@gmail.com>
2025-06-26 17:54:26 +0530

    dell

--- src/components/pages/AdminTickets.jsx
+++ src/components/pages/AdminTickets.jsx
@@ -258,7 +258,7 @@ function AdminTickets() {
                   : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
               }`}
             >
-              Closed Projects
+              Deleted Projects
             </button>
           </nav>
         </div>
